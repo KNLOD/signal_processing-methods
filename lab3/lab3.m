134f7bf66456efe59cbabea38702771d0cc7bf81
KNLOD <knlod@LAPTOP-V8HNB6MS.localdomain>
2021-10-13 12:46:52 +0300

    main programm + filtr_progr

--- lab3/lab3.m
+++ lab3/lab3.m
@@ -1,3 +1,19 @@
 clear all
 close all
+global hAxes1 hAxes2 hAxes3 hEda %объявление глобальных имен
 
+%Текстовый элемент с надписью "da=":
+
+hTxta=uicontrol('Style', 'text', 'String', 'da=',...
+      'Position',[x1,y1,w1,h1], 'Backgroundcolor', [1 1 1]);
+
+%Поле редактирования для ввода значения шага адаптации "da":
+
+hEda=uicontrol('Style', 'edit', 'Position', [x2, y2, w2, h2],...
+     'Backgroundcolor', [1 1 1], 'HorizantalAlignment', 'left');
+ 
+ %Кнопка с надписью 'Filter' для запуска процедуры фильтрации:
+ hBtn=uicontrol('Style', 'pushbutton', 'String', 'Filter', ...
+      'Positoin', [x3, y3, w3, h3], 'Callback', 'filt_progr');
+ 
+  
