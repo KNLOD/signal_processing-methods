5b27359fade24ac4e10aac0922c3ae77fdeb8440
Konstantin Novikov <vokivon@Konstantins-MacBook-Air.local>
2021-11-24 11:06:36 +0300

    lab5

--- lab5/filtr_progr.m
+++ lab5/filtr_progr.m
@@ -1,5 +1,15 @@
-
+global hAxes1 hAxes2 hAxes3 hEda %объявление глобальных имен
 
 str=get(hEda,'String');
-da=num2str(str);
+da=str2num(str);
+
+
+x=load('W5_01.txt');
+
+axes(hAxes1)
+plot(x);
+
+axes(hAxes2)
 y=fs50(x,Fs,da);
+plot(y);
+
--- lab5/lab5.m
+++ lab5/lab5.m
@@ -5,14 +5,14 @@ global hAxes1 hAxes2 hAxes3 hEda %объявление глобальных им
 X0=100;
 Y0=100;
 W0=300;
-H0=200;
+H0=300;
 figure('Position', [X0,Y0,W0,H0]);
 
 X1=10;
 Y1=10;
 Dy=20;
-W1=100;
-H1=50;
+W1=200;
+H1=100;
 
 hAxes1=axes('Units','Pixels','Position',[X1,Y1,W1,H1]);
 hAxes2=axes('Units','Pixels', 'Position',[X1,Y1+Dy+H1,W1,H1]);
@@ -20,7 +20,7 @@ hAxes3=axes('Units','Pixels', 'Position',[X1,Y1+2*Dy+2*H1,W1,H1]);
 
 
 %Текстовый элемент с надписью "da=":
-x1=100;
+x1=150;
 y1=30;
 w1=50;
 h1=50;
@@ -31,18 +31,18 @@ hTxta=uicontrol('Style', 'text', 'String', 'da=',...
 
 %Поле редактирования для ввода значения шага адаптации "da":
 
-x2=100;
+x2=150;
 y2=100;
 w2=50;
 h2=50;
 hEda=uicontrol('Style', 'edit', 'Position', [x2, y2, w2, h2],...
      'Backgroundcolor', [1 1 1], 'HorizontalAlignment', 'left');
- x3=100;
- y3=200;
+ x3=150;
+ y3=150;
  w3=50;
  h3=50;
  %Кнопка с надписью 'Filter' для запуска процедуры фильтрации:
  hBtn=uicontrol('Style', 'pushbutton', 'String', 'Filter', ...
-      'Position', [x3, y3, w3, h3], 'Callback', 'filt_progr');
+      'Position', [x3, y3, w3, h3], 'Callback', 'filtr_progr');
  
-  
+
