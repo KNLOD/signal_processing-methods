b910a355046a6bcb4bef35fe8305d42490f86125
Konstantin Novikov <vokivon@Konstantins-MacBook-Air.local>
2021-11-24 12:50:41 +0300

    lab5 ended

--- lab5/filtr_progr.m
+++ lab5/filtr_progr.m
@@ -1,15 +1,34 @@
-global hAxes1 hAxes2 hAxes3 hEda %объявление глобальных имен
+global hAxes1 hAxes2 hAxes3 hEda c %объявление глобальных имен
 
 str=get(hEda,'String');
 da=str2num(str);
 
+if c.Value==1
+    x=load('W5_01.txt');
+    Fs=250
+    axes(hAxes3)
+    plot(x);
 
-x=load('W5_01.txt');
+    axes(hAxes2)
+    y=fs50(x,Fs,da);
+    plot(y);
+    for i=1:length(x)
+        m(i)=y(i)-x(i)
+    end
+    axes(hAxes1)
+    plot(m);
+end
+if c.Value==2
+    Fs=500
+    t=[0:1/Fs:0.5]
+    x1=sin(2*pi*50*t)
+    axes(hAxes3)
+    plot(t,x1);
 
-axes(hAxes1)
-plot(x);
-
-axes(hAxes2)
-y=fs50(x,Fs,da);
-plot(y);
-
+    axes(hAxes2)
+    y=fs50(x1,Fs,da);
+    plot(t,y);
+    
+    axes(hAxes1)
+    plot(t,y-x1)
+end
--- lab5/lab5.m
+++ lab5/lab5.m
@@ -1,18 +1,21 @@
 clear all
 close all
-global hAxes1 hAxes2 hAxes3 hEda %объявление глобальных имен
+global hAxes1 hAxes2 hAxes3 hEda c%объявление глобальных имен
+
+Fs=250;
+
 
 X0=100;
 Y0=100;
-W0=300;
-H0=300;
-figure('Position', [X0,Y0,W0,H0]);
+W0=500;
+H0=600;
+f=figure('Position', [X0,Y0,W0,H0]);
 
 X1=10;
 Y1=10;
 Dy=20;
-W1=200;
-H1=100;
+W1=400;
+H1=200;
 
 hAxes1=axes('Units','Pixels','Position',[X1,Y1,W1,H1]);
 hAxes2=axes('Units','Pixels', 'Position',[X1,Y1+Dy+H1,W1,H1]);
@@ -20,8 +23,9 @@ hAxes3=axes('Units','Pixels', 'Position',[X1,Y1+2*Dy+2*H1,W1,H1]);
 
 
 %Текстовый элемент с надписью "da=":
-x1=150;
-y1=30;
+x1=450;
+y1=300;
+
 w1=50;
 h1=50;
 
@@ -31,18 +35,35 @@ hTxta=uicontrol('Style', 'text', 'String', 'da=',...
 
 %Поле редактирования для ввода значения шага адаптации "da":
 
-x2=150;
-y2=100;
+x2=450;
+y2=300;
 w2=50;
-h2=50;
+h2=25;
 hEda=uicontrol('Style', 'edit', 'Position', [x2, y2, w2, h2],...
      'Backgroundcolor', [1 1 1], 'HorizontalAlignment', 'left');
- x3=150;
+ x3=450;
  y3=150;
  w3=50;
  h3=50;
  %Кнопка с надписью 'Filter' для запуска процедуры фильтрации:
  hBtn=uicontrol('Style', 'pushbutton', 'String', 'Filter', ...
       'Position', [x3, y3, w3, h3], 'Callback', 'filtr_progr');
- 
+
+c=mytemps(f,x3,y1+100,50,20)
+c.Value
+function c=mytemps(f,x,y,w,h)
+
+c = uicontrol(f,'Style','popupmenu');
+c.Position = [x y w h];
+c.String = {'ECG','Sin'};
+c.Callback = @selection;
+
+    function selection(src,event)
+        val = c.Value;
+        str = c.String;
+        str{val};
+        disp(['Selection: ' str{val}]);
+    end
+
+end
 
